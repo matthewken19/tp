249bb4010a1d643a072a5cdfa899fd0d8a5976d4
TanJiaJun <e0725056@u.nus.edu>
2024-04-12 02:55:32 +0800

    Fix checkstyletest

--- src/test/java/educonnect/model/student/LinkTest.java
+++ src/test/java/educonnect/model/student/LinkTest.java
@@ -1,10 +1,10 @@
 package educonnect.model.student;
 
-import org.junit.jupiter.api.Test;
-
 import static educonnect.testutil.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 public class LinkTest {
     @Test

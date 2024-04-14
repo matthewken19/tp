aee0fd081c15c350b6feda65210ce887b830da31
TanJiaJun <e0725056@u.nus.edu>
2024-04-14 22:55:47 +0800

    Update testcases for URLs

--- src/main/java/educonnect/model/student/Link.java
+++ src/main/java/educonnect/model/student/Link.java
@@ -9,11 +9,24 @@ import static java.util.Objects.requireNonNull;
 public class Link {
 
     public static final String MESSAGE_CONSTRAINTS = "Please provide a valid website.";
-    //    @@author tanjiajiajun-reused
-    //    Reused from https://stackoverflow.com/questions/42618872/regex-for-website-or-url-validation,
-    //    under Mustofa Rizwan's response
-
-    public static final String VALIDATION_REGEX = "^(?<scheme>(?:ftp|https?):\\/\\/)?+(?:(?<username>[a-zA-Z][\\w-.]{0,31})(?::(?<password>[!-~&&[^@$\\n\\r]]{6,255}))?@)?(?<subdomain>(?:[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9]?\\.){0,127})(?<domain>[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9])(?<tld>\\.[a-zA-Z]{3,63})(?<cctld>\\.[a-zA-Z]{2})?(?<portnumber>:\\d{1,5})?(?<path>(?:\\/{1,2}[\\w-@.~()%]*)*)(?<querystring>\\?(?:[\\w-%]+=[\\w-?/:@.~!$&'()*+,;=%]+(?:&[\\w-%]+=[\\w-?/:@.~!$&'()*+,;=%]+)*)?)?(?<fragment>#[\\w-?/:@.~!$&'()*+,;=%]+)?$";
+    // RegEx groups match plan:
+    // 1) scheme ftp/http/https
+    // 2) ssh username
+    // 3) ssh password
+    // 4) sub-domain(s) including www.
+    // 5) domain name
+    // 6) gTLD (generic top-level domain) e.g. .com
+    // 7) ccTLD (country-code top-level domain) e.g. .sg
+    // 8) port number e.g. 8080
+    // 9) path e.g. /file/file
+    // 10) query string e.g. ?something
+    // 11) fragment e.g. #something
+    public static final String VALIDATION_REGEX = "^(?<scheme>(?:ftp|https?):\\/\\/)?+(?:(?<username>[a-zA-Z][\\w-.]"
+            + "{0,31})(?::(?<password>[!-~&&[^@$\\n\\r]]{6,255}))?@)?(?<subdomain>(?:[a-zA-Z0-9][a-zA-Z0-9-]{0,61}"
+            + "[a-zA-Z0-9]?\\.){0,127})(?<domain>[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9])(?<tld>\\."
+            + "[a-zA-Z]{3,63})(?<cctld>\\.[a-zA-Z]{2})?(?<portnumber>:\\d{1,5})?(?<path>(?:\\/{1,2}[\\w-@.~()%]"
+            + "*)*)(?<querystring>\\?(?:[\\w-%]+=[\\w-?/:@.~!$&'()*+,;=%]+(?:&[\\w-%]+=[\\w-?/:@.~!$&'()*+,;=%]+)*)"
+            + "?)?(?<fragment>#[\\w-?/:@.~!$&'()*+,;=%]+)?$";
     //@@author
     public final String url;
 
